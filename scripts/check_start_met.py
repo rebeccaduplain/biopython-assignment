605250edbcaa444e8b6c24033837ddf2c119f806
rebeccaduplain <117837268+rebeccaduplain@users.noreply.github.com>
2022-11-30 23:35:30 -0500

    Add files via upload

---
+++ scripts/check_start_met.py
@@ -0,0 +1,8 @@
+from Bio import SeqIO
+filename = "NC_000913.faa"
+bad = 0
+for record in SeqIO.parse(filename, "fasta"):
+    if not record.seq.startswith("M"):
+        bad = bad + 1
+        print(record.id + " starts " + record.seq[0])
+print("Found " + str(bad) + " records in " + filename + " which did not start with M")
